523bf153703fa41daa2a97e6d01f5c64ff82af0d
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-03-13 13:00:49 +0530

    fix: config path

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,6 @@ export async function middleware(request: NextRequest) {
   });
 }
 
-export const config = {
-  matcher: ["/quiz", "/login"],
-};
+// export const config = {
+//   matcher: ["/quiz", "/login"],
+// };
