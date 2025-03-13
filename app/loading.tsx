d2fca7e06d3241461fd15bc674156155c87cf369
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-03-13 12:13:12 +0530

    feat: home loading

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+// import PageSkeleton from "@/components/page-skeleton"
+
+import PageSkeleton from "@/components/skeletons/page-skeleton";
+
+export default function Loading() {
+  return <PageSkeleton />
+}
+
