e91ce23cc094fcfbe298c8563b650074e788a7cf
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-03-13 14:21:50 +0530

    fix: auth

--- middleware.ts
+++ middleware.ts
@@ -6,19 +6,17 @@ const protectedRoutes = [QUIZ_ROUTE];
 // Define the matcher configuration
 
 
-export default function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || '';
 
   // Redirect to login if session is not set
   if (!session && protectedRoutes.includes(request.nextUrl.pathname)) {
-    const absoluteURL = new URL(ROOT_ROUTE, request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
+    return NextResponse.redirect(new URL(ROOT_ROUTE, request.nextUrl));
   }
 
   // Redirect to home if session is set and user tries to access root
   if (session && request.nextUrl.pathname === ROOT_ROUTE) {
-    const absoluteURL = new URL(ROOT_ROUTE, request.nextUrl.origin);
-    return NextResponse.redirect(absoluteURL.toString());
+    return NextResponse.redirect(new URL(QUIZ_ROUTE, request.nextUrl));
   }
 
   return NextResponse.next();
