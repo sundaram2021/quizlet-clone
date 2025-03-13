673c7e836c2168730d5e1136a00c2c8383a8406d
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-03-13 12:13:24 +0530

    feat: home page

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PDFQuizGeneratorHeader from '@/components/home';
+
+const Home = () => {
+    return (
+        <PDFQuizGeneratorHeader />
+    );
+};
+
+export default Home;
