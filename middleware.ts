32eaca2799efaefe6efca33c2283f4df976b8e8c
Sundaram Kumar Jha <jhasundaram@outlook.com>
2025-03-13 13:30:37 +0530

    fix: auth

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import { type NextRequest, NextResponse } from 'next/server';
-import {  ROOT_ROUTE, SESSION_COOKIE_NAME, QUIZ_ROUTE } from './lib/constants';
+import { ROOT_ROUTE, SESSION_COOKIE_NAME, QUIZ_ROUTE } from './lib/constants';
 
 const protectedRoutes = [QUIZ_ROUTE];
+
+// Add a matcher configuration
+export const config = {
+  matcher: ['/quiz', '/'], // Ensure each entry starts with a '/'
+};
 
 export default function middleware(request: NextRequest) {
   const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || '';
@@ -14,7 +19,7 @@ export default function middleware(request: NextRequest) {
 
   // Redirect to home if session is set and user tries to access root
   if (session && request.nextUrl.pathname === ROOT_ROUTE) {
-    const absoluteURL = new URL(ROOT_ROUTE, request.nextUrl.origin);
+    const absoluteURL = new URL(QUIZ_ROUTE, request.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
 }
